a21c7aeba7815375cd6731db4556dbc00f6e6edc
Danny Morrow <morrowdt@goldmail.etsu.edu>
2016-02-08 20:28:04 -0500

    Added basic login features

---
+++ LoginForm.cs
@@ -0,0 +1,51 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForms
+{
+    public partial class LoginForm : Form
+    {
+        MainForm Parent;
+
+        public LoginForm(MainForm parentForm)
+        {
+            Parent = parentForm;
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            string user = UsernameTextbox.Text;
+            string pass = PasswordTextbox.Text;
+
+            string validateUser = "SELECT user_id FROM user_accounts WHERE username = '" + user + "' AND password = '" + pass + "'";
+
+            MySqlCommand command = new MySqlCommand(validateUser, Parent.Connection);
+
+            int value = Convert.ToInt32(command.ExecuteScalar());
+
+            if (value != 0)
+            {
+                MySqlDataReader rdr = command.ExecuteReader();
+                rdr.Read();
+                int userId = int.Parse(rdr.GetString(0));
+                rdr.Close();
+
+                this.Hide();
+                Parent.ShowInfo(userId);
+            }
+            else
+            {
+                ErrorLabel.Visible = true;
+            }
+        }
+    }
+}
