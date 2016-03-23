e47efcd6d32b386eecc94058dd18510d7ea5fa10
Stan Seiferth <sjseife@gmail.com>
2016-03-23 10:29:06 -0400

    stan

--- ComboSelection.Designer.cs
+++ ComboSelection.Designer.cs
@@ -74,6 +74,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(468, 252);
+            this.ControlBox = false;
             this.Controls.Add(this.label1);
             this.Controls.Add(this.addCombo);
             this.Controls.Add(this.textCollectionBox);
